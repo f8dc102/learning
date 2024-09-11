c9a053ead8247cc8a98075b456ca518f3ff2b187
f91ce1d <kernel@yonsei.ac.kr>
2024-09-11 06:50:10 +0000

    Daily Practice

--- Java/Basic/ClassExample.java
+++ Java/Basic/ClassExample.java
@@ -1,4 +1,4 @@
-public class Main {
+public class ClassExample {
   public static void main(String[] args) {
     aCat testCat = new aCat();
 
--- Java/Basic/September.java
+++ Java/Basic/September.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
-public class Sep9 {
+/**
+ * September Class
+ */
+public class September {
   /**
    * Main Method
    * 
