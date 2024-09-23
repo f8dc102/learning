3b40c2df792c64dfc761e74dc28c977a08db13a1
f91ce1d <kernel@yonsei.ac.kr>
2024-09-23 04:46:06 +0000

    Daily Practice

---
+++ Java/Spring/sample/src/test/java/com/f8dc747/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f8dc747.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
