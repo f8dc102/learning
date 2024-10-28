0f4410f45ec3f97c48013c2614f3104c4a2efe80
F8DC102 <kernel@yonsei.ac.kr>
2024-10-28 06:59:43 +0000

    Daily Practice

---
+++ packages/java/Basic/Chapter6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Chapter6 {
+  public static void main(String[] args) {
+    // Choose class to run
+    Scanner userInput = new Scanner(System.in);
+
+    // Close Scanner
+    userInput.close();
+  }
+}
+
+class Inheritance {
+
+}
+
+class Polymorphism {
+
+}
+
+class Encapsulation {
+
+}
