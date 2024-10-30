98c37b7725e7bedd3ac0cd52080eb550f314eab3
F8DC102 <kernel@yonsei.ac.kr>
2024-10-30 07:10:39 +0000

    Daily Practice

--- packages/java/Basic/Chapter6.java
+++ packages/java/Basic/Chapter6.java
@@ -159,5 +159,21 @@ class Practice {
         return Integer.toString(a);
       }
     }
+
+    class Manager extends Employee {
+      private int bonus;
+
+      public Manager(String name, String address, int salary, int rrn, int bonus) {
+        super(name, address, salary, rrn);
+        this.bonus = bonus;
+      }
+
+      public void test() {
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(salary);
+        // Error: System.out.println(rrn);
+      }
+    }
   }
 }
