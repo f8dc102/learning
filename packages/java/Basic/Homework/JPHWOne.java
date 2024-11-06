94377edc0bf005954f1090e2f0413a8cc0c4516b
F8DC102 <kernel@yonsei.ac.kr>
2024-11-06 06:03:34 +0000

    Daily Practice

--- packages/java/Basic/Homework/JPHWOne.java
+++ packages/java/Basic/Homework/JPHWOne.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HWOne {
+public class JPHWOne {
   public static void main(String[] args) {
     // Create a Scanner object
     Scanner userInput = new Scanner(System.in);
